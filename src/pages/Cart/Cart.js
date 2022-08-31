13817323943b94b0f023e414737f0a05882464a1
zionii <tjwldnjs12345@naver.com>
2022-08-31 13:58:32 +0900

    Add: Cart레이아웃 생성

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div className="Cart-container">
+      <div className="Cart-title">
+        <h2>CART</h2>
+      </div>
+      <div>
+        <di>
+          배송상품(<span>2</span>)
+        </di>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
