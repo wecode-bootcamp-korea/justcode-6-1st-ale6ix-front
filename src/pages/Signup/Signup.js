7e895fa0577d9ce6d4cb24fe28f4f7d6b1768bbc
choongmankim <cm730712@gmail.com>
2022-09-02 20:49:41 +0900

    Add: signup 페이지 레이아웃 시작

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,15 @@
-import React from 'react';
-
-import css from './Signup.module.scss';
+import React from "react";
+import "./Signup.scss";
 
 function Signup() {
-  return <div className={css.container}></div>;
+  return (
+    <div className="signup-wrap">
+      <main className="signup-container">
+        <h2 className="signup-title">회원 가입</h2>
+        <h3 className="basic-info">기본정보</h3>
+      </main>
+    </div>
+  );
 }
 
 export default Signup;
