298c19b64f224d9bc6268e59a103a4fe4721607c
choongmankim <cm730712@gmail.com>
2022-09-02 21:09:26 +0900

    Add: signup 페이지 컴포넌트 사용 후 레이아웃 재정비

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Signup.scss";
+import InputForm from "../../components/Signups/InputForm";
+import Terms from "../../components/Signups/Terms";
+import AddInfo from "../../components/Signups/AddInfo";
 
 function Signup() {
   return (
@@ -7,6 +11,16 @@ function Signup() {
       <main className="signup-container">
         <h2 className="signup-title">회원 가입</h2>
         <h3 className="basic-info">기본정보</h3>
+        <InputForm />
+        <h3 className="add-info">추가정보</h3>
+        <AddInfo />
+        <h3 className="whole-agreement">전체 동의</h3>
+        <Terms />
+        <div className="signup-btn" type="submit">
+          <Link className="go-to-main" to="/">
+            회원가입
+          </Link>
+        </div>
       </main>
     </div>
   );
