a7995f3b4e7048834339ce996a6f8bba43c1a1d6
miogy <jjangrl87@gmail.com>
2022-09-02 20:36:06 +0900

    ADD: nav 완료

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import css from './Header.module.scss';
+import "./Header.scss";
+import SubNav from "./SubNav";
 
 function Header() {
-  return <div className={css.container}></div>;
+
+  const [nav,setNav] = useState([]);
+
+  useEffect(()=>{
+    fetch('/data/navCategories.json')
+    .then((res)=>res.json())
+    .then((data)=>{
+      setNav(data.categoryData)
+    })
+  },[])
+
+  return (
+  <div className="container">
+    <div>
+      <ul className="nav-top">
+        <li className="nav-top-menu"><a href="#" className="nav-top-font">LOGIN</a></li>
+        <li className="nav-top-menu"><a href="#" className="nav-top-font">JOIN</a></li>
+        <li className="nav-top-menu"><a href="#" className="nav-top-font">CART <span className="counter">0</span></a></li>
+        <li className="nav-top-menu"><a href="#" className="nav-top-font">username</a></li>
+        <li>
+          <button>
+          <a href="#">
+            <img src="http://alessi.co.kr/_dj/img/top_r_menu_icon_03_b.png" alt="검색" />
+          </a>
+          </button></li>
+      </ul>
+    </div>
+    <div className="nav-bottom">
+      <h1>ALE6IX</h1>
+      <ul className="nav-menu">
+        <li><a href="#" className="nav-font">ABOUT</a></li>
+        <li><a href="#" className="nav-font">STORE</a></li>
+          {nav.map((navlist)=>{
+            return <li 
+              key={navlist.LargeCategoryId}
+              className="data-nav"
+              >
+                {navlist.LargeCategoryName}
+              <div className="sub-nav-pop">
+                <SubNav sublist={navlist.smallCategories}/>
+              </div>
+              </li>
+            })}
+        
+      </ul>
+      
+    </div>
+    
+  </div>
+  )
 }
 
 export default Header;
---
+++ src/components/Header/SubNav.js
@@ -0,0 +1,18 @@
+import './SubNav.scss'
+
+function SubNav({sublist}){
+  return(
+    <div className="sub-nav">
+      <ul className="sub-menu">
+        {sublist.map((subNav)=>{
+          return <li>{subNav.smallCategoryName}</li>
+        })}
+        
+      </ul>
+      <div className='sub-menu-img'>
+        <img src="http://alessi.co.kr/_dj/img/category_item_img_1.jpg" alt="img"></img>
+      </div>
+    </div>
+  )
+}
+export default SubNav;
