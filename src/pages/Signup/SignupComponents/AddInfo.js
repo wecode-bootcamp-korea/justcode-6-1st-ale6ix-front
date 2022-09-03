7bd7837e99676e8f8b53dd3d25a29962b94d846e
choongmankim <cm730712@gmail.com>
2022-09-03 23:06:50 +0900

    Add: 회원가입 추가정보 input type='number' 변경

--- src/pages/Signup/SignupComponents/AddInfo.js
+++ src/pages/Signup/SignupComponents/AddInfo.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './AddInfo.scss';
+import React from "react";
+import "./AddInfo.scss";
 
 function AddInfo() {
   return (
     <div className="signup-birth">
       <span className="width">생년월일</span>
-      <input alt="생년월일" className="join-birth-year" type="text" />
+      <input alt="생년월일" className="join-birth-year" type="number" />
       <span className="birth">&nbsp;년 </span>
-      <input alt="생년월일" className="join-birth-month" type="text" />
+      <input alt="생년월일" className="join-birth-month" type="number" />
       <span className="birth">&nbsp;월 </span>
-      <input alt="생년월일" className="join-birth-day" type="text" />
+      <input alt="생년월일" className="join-birth-day" type="number" />
       <span className="birth">&nbsp;일 </span>
       <span>
         &nbsp;&nbsp;&nbsp;
