be262ebf153155d780293065b6d533a5d72c9ff8
lkw1996 <lkw3703@gmail.com>
2022-09-06 21:32:42 +0900

    에러 수정

--- src/pages/Product/ProductBigCategory.js
+++ src/pages/Product/ProductBigCategory.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 import ProductCategory from "./ProductCategory";
 import ProductList from "./ProductList";
+import Paginate from "./Paginate"
 import "./Product.scss";
 
 function ProductBigCategory() {
