6ee511301eb4084ed905683940bdcb07ccebaa7a
miogy <jjangrl87@gmail.com>
2022-09-06 15:41:05 +0900

    ADD: Review기능

--- src/pages/Product/Review.js
+++ src/pages/Product/Review.js
@@ -1,8 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 
 function Review(){
-
-  const value = useRef();
 
   const [reveiw,setReview]=useState([
     {
@@ -13,78 +11,96 @@ function Review(){
     }
   ])
 
-  const [newReview,setNewReview]=useState();
+  //ref
+  const titleValue = useRef();
+  const textValue = useRef();
 
-  //add
-  const [id,setId]=useState(1);
+//add list => get
+  const [newReview,setNewReview]=useState([]);
 
-  const addlist = ()=>{
-    setId(id+1)
-    const newList = {
-      id : id,
-      title : title,
-      // userName : userName,
-      content : value.current.value,
-      createdAt : new Date().toLocaleString()
-    }
-    value.current.value = ""; 
-    setReview([...reveiw,newList])
-  }
-  //enter
-  const enterEvent = (e)=>{
-    if(e.key === 'Enter'){ 
-      addlist();
-    }
-  }
 
-  const [title,setTitle] = useState('');
-  
-  const body={
-    product_id : id,
-    title : title ,
-    content : value.current.value
-  }
+  useEffect(()=>{
 
-  
-  const addReview = ()=>{
-    fetch("http://localhost:4000/products/reviews",{
-      method:"POST",
+    fetch("http://localhost:4000/products/detail/1",{
+      method:"GET",
       headers:{
-	      Authorization : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjM2LCJpYXQiOjE2NjIzNjI4NTUsImV4cCI6MTY2MjM3MzY1NX0._9kopi-Xzq0HJP1yQXE9wdyR_1w8fbxwVRV2nFpELRM'
-      },
-      body : JSON.stringify(body)
-    })
-    .then((res)=>{
-      if(res.ok){
-        alert('리뷰 등록이 완료되었습니다.');
-      }else {
+        "Content-Type" : "application/json"
       }
     })
-    // .then((data)=>{
-    //   fetch("http://localhost:4000/products/reviews",{
-    //     headers:{
-    //       Authorization : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjM2LCJpYXQiOjE2NjIzNjI4NTUsImV4cCI6MTY2MjM3MzY1NX0._9kopi-Xzq0HJP1yQXE9wdyR_1w8fbxwVRV2nFpELRM'
-    //     },
-    //   })
-    //   .then((res)=>res.json())
-    //   .then((data)=>{
-    //     setNewReview(data)
-    //   })
-    // })
-    .catch(error => console.error(error.message));
-  }
+    .then(res => res.json())
+    .then((reveiwData)=>{
+      // console.log(reviewData);
+      setReview([...reveiw,reveiwData])
+    })
+  },[newReview])
+
+    // setId(id+1)
+    // const newList = {
+    //   id : id,
+    //   title : titleValue.current.value ,
+    //   // userName : userName,
+    //   content : textValue.current.value,
+    //   createdAt : new Date().toLocaleString()
+    // }
+    // titleValue.current.value = "";
+    // textValue.current.value = ""; 
+    // setReview([...reveiw,newList])
   
 
+//enter
+  const enterEvent = (e)=>{
+    if(e.key === 'Enter'){ 
+      addReview();
+    }
+  }
+  
+//add review => post
+
+  const [id,setId]=useState(1);
+
+  const addReview = ()=>{
+    if(reveiw.length > 0){
+      setId(id+1)
+      const body={
+        title : titleValue.current.value,
+        content : textValue.current.value
+      }
+      // let token = localStorage.getItem("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjM2LCJpYXQiOjE2NjI0MzkyODYsImV4cCI6MTY2MjQ1MDA4Nn0.2fpuIsqMyECkvivKnyzgkUFCj22SaGI6rX2zhDxtplU") || '' ;
+
+      fetch("http://localhost:4000/products/detail/1/review",{
+        method:"POST",
+        headers:{
+          "Content-Type" : "application/json",
+          "Authorization" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjM2LCJpYXQiOjE2NjI0MzkyODYsImV4cCI6MTY2MjQ1MDA4Nn0.2fpuIsqMyECkvivKnyzgkUFCj22SaGI6rX2zhDxtplU"
+        },
+        body : JSON.stringify(body)
+      })
+      //보내기
+      .then(res => res.json())
+      .then(result=>{
+        if(result.message == "post created"){
+          
+          setNewReview();
+          console.log('post created')
+        }
+        // console.log(result);
+      })
+
+    }
+  }
+    
   return(
     <div>
       <div className="review-container">
         <ul>
-          {newReview.length > 0 &&
-          newReview.map((list,index)=>{
-            return <li key={index} 
+          {newReview &&
+          newReview.map((list)=>{
+            return <li key={list.id} 
             className="reviewCard">
             <div className="row">
-              <div>{list.title}</div>
+              <div>
+              {list.title}
+              </div>
               <h3 className="userId">{list.account}</h3>
               <span className="createdTime">
                 {list.createdAt}
@@ -95,8 +111,10 @@ function Review(){
           })}
         </ul>
       </div>
+      <input type="text" 
+             ref={titleValue} />
       <input 
-      ref={value}
+      ref={textValue}
       onKeyDown={enterEvent}
       // onChange={(e)=>{
       //   setNewReview(e.target.value)
@@ -107,5 +125,6 @@ function Review(){
       >저장</button>
     </div>
   )
+
 }
 export default Review;
