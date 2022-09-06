0d3b321637775905dc25e0b71dbf04e90c8e874f
lkw1996 <lkw3703@gmail.com>
2022-09-06 20:19:19 +0900

    [Add] 동적 라우팅 추가

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -2,24 +2,32 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Aside from "../components/Aside/Aside";
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer"
+import Footer from "../components/Footer/Footer";
 import Cart from "./Cart/Cart";
-
-import Home from "./Home/Home";
 import Login from "./Login/Login";
 import Main from "./Main/Main";
 import Signup from "./Signup/Signup";
+import ProductBigCate from "./Product/ProductBigCate";
+import ProductSmallCate from "./Product/ProductSmallCate";
+import ProductDetail from "./Product/ProductDetail";
 
 function Router() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/carts" element={<Cart />} />
+        <Route path="/product/:bigId" element={<ProductBigCate />} />
+        <Route path="/product/:bigId/:smallId" element={<ProductSmallCate />} />
+        <Route
+          exact
+          path="/product/detail/:productId"
+          element={<ProductDetail />}
+        />
       </Routes>
       <Aside />
       <Footer />
