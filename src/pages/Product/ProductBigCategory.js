23c770b085d16ed68cbe2a7192fd367d95c9052c
lkw1996 <lkw3703@gmail.com>
2022-09-06 19:51:12 +0900

    [Add] data fetch 이후 state에 저장

--- src/pages/Product/ProductBigCategory.js
+++ src/pages/Product/ProductBigCategory.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function ProductBigCategory() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:8000/products/list?groupCodeId=${bigId}&limit=10&offset=${offset}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [bigId, offset]);
+
   return;
 }
 
--- src/pages/Product/ProductSmallCategory.js
+++ src/pages/Product/ProductSmallCategory.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function ProductSmallCategory() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:8000/products/list?groupCodeId=${bigId}&detailCodeId=${smallId}&limit=10&offset=${offset}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [bigId, smallId, offset]);
+
   return;
 }
 
