0b881642a6e7c35e1039f3480b2a5d590a3b6338
lkw1996 <lkw3703@gmail.com>
2022-09-06 19:47:36 +0900

    [Add] 프로덕트 파일 구조 추가

---
+++ src/pages/Product/ProductBigCategory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductBigCategory() {
+  return;
+}
+
+export default ProductBigCategory;
---
+++ src/pages/Product/ProductSmallCategory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductSmallCategory() {
+  return;
+}
+
+export default ProductSmallCategory;
