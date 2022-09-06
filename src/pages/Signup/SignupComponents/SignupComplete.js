0605b8a988e2152f42ce50bc4937a863e35430fb
choongmankim <cm730712@gmail.com>
2022-09-06 11:48:40 +0900

    Add: 회원가입 완료 페이지 추가

---
+++ src/pages/Signup/SignupComponents/SignupComplete.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SignupComplete.scss';
+
+function SignupComplete () {
+  return (
+    <div className='wrap'>
+      <div className='signupComplete'>
+        <h2>회원 가입 완료</h2>
+        <div>회원가입이 완료 되었습니다.</div>
+        <div>
+        </div>
+      </div>
+    </div>
+  )
+} 
+
+export default SignupComplete;
