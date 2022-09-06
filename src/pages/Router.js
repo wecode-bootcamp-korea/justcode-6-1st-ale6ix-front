82d9c8278591279aa3bca7569010730b8a93dcb9
choongmankim <cm730712@gmail.com>
2022-09-06 18:02:18 +0900

    Add: 회원가입 완료 페이지 경로 추가

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -2,23 +2,25 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Aside from "../components/Aside/Aside";
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer"
+import Footer from "../components/Footer/Footer";
 import Cart from "./Cart/Cart";
 
 import Home from "./Home/Home";
 import Login from "./Login/Login";
 import Main from "./Main/Main";
 import Signup from "./Signup/Signup";
+import SignupComplete from "./Signup/SignupComponents/SignupComplete";
 
 function Router() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signup/complete" element={<SignupComplete />} />
         <Route path="/carts" element={<Cart />} />
       </Routes>
       <Aside />
