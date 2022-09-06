8c93856c2b4083eb1afc985ed7e5f6213ee39028
lkw1996 <lkw3703@gmail.com>
2022-09-06 20:08:21 +0900

    [Modify]맵 함수를 이용한 레이아웃 변경

--- src/pages/Product/ProductList.js
+++ src/pages/Product/ProductList.js
@@ -14,13 +14,17 @@ function ProductList(props) {
       <div className="product-list-container">
         {props.subCategoryInfo.map((subInfo) => {
           return (
-            <div className="product-list">
+            <div
+              className="product-list"
+              key={subInfo.id}
+              onClick={() => clickHandler(subInfo.id)}
+            >
               <span className="product-image">
                 <img src={subInfo.main_image_url} alt="이미지" />
               </span>
               <span className="product-title">{subInfo.product_name}</span>
               <span className="product-price">
-                {Number(subInfo.price).toLocaleString()}
+                {Number(subInfo.price).toLocaleString() + " won"}
               </span>
             </div>
           );
